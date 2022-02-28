a3f5007250195d18873380801122d11fe970c16e
rodoliver <wyld.youth@yahoo.com>
2022-02-28 05:52:26 +0000

    Finish layout and routes

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,9 +9,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     get signup_path    
-    get contact_path
+    #get contact_path
     
-    assert_select "title", full_title("Contact")
-    assert_select "title", full_title("Sign Up")
+    assert_select "title", full_title("Sign up")
+    #assert_select "title", full_title("Contact")
    end
 end

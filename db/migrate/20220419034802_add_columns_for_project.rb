6754b325a73fa49f3289d7ba312fbea455f85851
Kilik White <kilikwhite@outlook.com>
2022-04-19 04:51:47 +0000

    added more columns in database (INCOMPLETE)

---
+++ db/migrate/20220419034802_add_columns_for_project.rb
@@ -0,0 +1,12 @@
+class AddColumnsForProject < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :bDay, :date, default:2022-12-31
+    add_column :users, :admin, :boolean, default:false
+    add_column :users, :address, :string
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+    add_column :users, :zip, :integer
+    add_column :users, :pNumber, :string
+    add_column :users, :username, :string
+  end
+end
